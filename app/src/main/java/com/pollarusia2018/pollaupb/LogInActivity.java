ef8739a88c0d00782298fcc91c9e7878f117d4e3
Daniel Alvarez <a3dany@gmail.com>
2017-11-28 10:00:42 -0400

    Firebase Authentication Email

--- app/src/main/java/com/pollarusia2018/pollaupb/LogInActivity.java
+++ app/src/main/java/com/pollarusia2018/pollaupb/LogInActivity.java
@@ -2,11 +2,18 @@ package com.pollarusia2018.pollaupb;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -16,6 +23,8 @@ public class LogInActivity extends AppCompatActivity {
     private Button openRegisterButton;
 
     private ProgressBar progressBar;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +48,37 @@ public class LogInActivity extends AppCompatActivity {
             }
         });
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 
     private void logIn() {
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
 
+        progressBar.setVisibility(View.VISIBLE);
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressBar.setVisibility(View.GONE);
+                        if (task.isSuccessful()) {
+                            goMainScreen();
+                        } else {
+                            showErrorMessage(task.getException().toString());
+                        }
+                    }
+                });
+    }
+
+    private void showErrorMessage(String s) {
+        Toast.makeText(getApplicationContext(), "Hay error: " + s, Toast.LENGTH_SHORT).show();
+    }
+
+    private void goMainScreen() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
     private void openRegister() {
--- app/src/main/java/com/pollarusia2018/pollaupb/RegisterActivity.java
+++ app/src/main/java/com/pollarusia2018/pollaupb/RegisterActivity.java
@@ -1,11 +1,18 @@
 package com.pollarusia2018.pollaupb;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -15,6 +22,8 @@ public class RegisterActivity extends AppCompatActivity {
     private Button registerButton;
 
     private ProgressBar progressBar;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +41,35 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 
     private void register() {
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
 
+        progressBar.setVisibility(View.VISIBLE);
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressBar.setVisibility(View.GONE);
+                        if (task.isSuccessful()) {
+                            goLogIn();
+                        } else {
+                            showErrorMessage(task.getException().toString());
+                        }
+                    }
+                });
+    }
+
+    private void showErrorMessage(String s) {
+        Toast.makeText(getApplicationContext(), "Hay error: " + s, Toast.LENGTH_LONG).show();
+    }
+
+    private void goLogIn() {
+        finish();
     }
 }
