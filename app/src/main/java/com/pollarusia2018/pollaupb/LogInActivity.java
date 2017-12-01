932460da2bf2a1b0a906d78c4006ed3f30b5a792
Adrian Arce <arce.diaz.adrian@gmail.com>
2017-12-01 08:02:12 -0400

    Google and Email Authentication

--- app/src/main/java/com/pollarusia2018/pollaupb/LogInActivity.java
+++ app/src/main/java/com/pollarusia2018/pollaupb/LogInActivity.java
@@ -2,13 +2,30 @@ package com.pollarusia2018.pollaupb;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class LogInActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class LogInActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private EditText emailEditText;
     private EditText passwordEditText;
@@ -16,6 +33,16 @@ public class LogInActivity extends AppCompatActivity {
     private Button openRegisterButton;
 
     private ProgressBar progressBar;
+
+    private FirebaseAuth firebaseAuth;
+
+    private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
+
+    private GoogleApiClient googleApiClient;
+
+    private SignInButton signInGoogleButton;
+
+    public static final int SING_IN_CODE = 777;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,14 +66,120 @@ public class LogInActivity extends AppCompatActivity {
             }
         });
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        firebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    goMainMenu();
+                }
+            }
+        };
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
+
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API, gso).build();
+
+        signInGoogleButton = findViewById(R.id.singInGoogleButton);
+        signInGoogleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent, 777);
+            }
+        });
     }
 
     private void logIn() {
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
 
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    goMainMenu();
+                } else {
+                    showErrorMessage(task.getException().toString());
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(firebaseAuthStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (firebaseAuthStateListener != null) {
+            firebaseAuth.removeAuthStateListener(firebaseAuthStateListener);
+        }
+    }
+
+    private void showErrorMessage(String s) {
+        Toast.makeText(getApplicationContext(), "Error de: " + s, Toast.LENGTH_SHORT).show();
+    }
+
+    private void goMainMenu() {
+        Intent i = new Intent(this, MainMenuActivity.class);
+        startActivity(i);
     }
 
     private void openRegister() {
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 777) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        if (result.isSuccess()) {
+            firebaseAuthWithGoogle(result.getSignInAccount());
+        } else {
+            Toast.makeText(this, "No se puede iniciar sesion", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount signInAccount) {
+        progressBar.setVisibility(View.VISIBLE);
+        signInGoogleButton.setVisibility(View.GONE);
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(signInAccount.getIdToken(), null);
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                progressBar.setVisibility(View.GONE);
+                signInGoogleButton.setVisibility(View.VISIBLE);
+
+                if(!task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(), R.string.not_firebase_auth, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/pollarusia2018/pollaupb/RegisterActivity.java
+++ app/src/main/java/com/pollarusia2018/pollaupb/RegisterActivity.java
@@ -1,11 +1,18 @@
 package com.pollarusia2018.pollaupb;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -15,6 +22,7 @@ public class RegisterActivity extends AppCompatActivity {
     private Button registerButton;
 
     private ProgressBar progressBar;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +40,39 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 
     private void register() {
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
 
+        progressBar.setVisibility(View.VISIBLE);
+        firebaseAuth.createUserWithEmailAndPassword(email,
+                password)
+                .addOnCompleteListener(this, new
+                        OnCompleteListener<AuthResult>() {
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    goLogIn();
+                                } else {
+                                    showErrorMessage(task.getException
+                                            ().toString());
+                                }
+                            }
+
+                        });
     }
+
+    private void showErrorMessage(String exception) {
+        Toast.makeText(getApplicationContext(), "Hay error: " +
+                exception, Toast.LENGTH_SHORT).show();
+    }
+
+    private void goLogIn() {
+        finish();
+    }
+
 }
