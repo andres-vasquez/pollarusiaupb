354881938ba2e560993c992a4dd2070143e5c203
Apita49 <jose.apa4996@gmail.com>
2017-12-04 09:05:22 -0400

    Este commit contiene el Profile Activity con imagenes circulares si es que existe la imagen, su respectivo manifest y la dependencia en graddle

--- app/src/main/java/com/pollarusia2018/pollaupb/ProfileActivity.java
+++ app/src/main/java/com/pollarusia2018/pollaupb/ProfileActivity.java
@@ -1,18 +1,31 @@
 package com.pollarusia2018.pollaupb;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class ProfileActivity extends AppCompatActivity {
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ProfileActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private TextView emailTextView;
+    private TextView nameTextView;
+    private TextView numberTextView;
+    private CircleImageView profileImageView;
+
+    private GoogleApiClient googleApiClient;
 
     private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +33,9 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         emailTextView = findViewById(R.id.emailTextView);
+        profileImageView = findViewById(R.id.profileImageView);
+        numberTextView =  findViewById(R.id.numberTextView);
+        nameTextView = findViewById(R.id.nameTextView);
 
         firebaseAuth = FirebaseAuth.getInstance();
 
@@ -31,12 +47,38 @@ public class ProfileActivity extends AppCompatActivity {
         if (firebaseUser != null) {
 
             String email = firebaseUser.getEmail();
+            String photoURL = firebaseUser.getPhotoUrl().toString();
+            String name = firebaseUser.getDisplayName();
+            String phone = firebaseUser.getPhoneNumber();
 
-            emailTextView.setText(email);
+            if(email!=null) {
+                emailTextView.setText(email);
+            }
+            if(name!=null) {
+                nameTextView.setText(name);
+            }
+            if(phone!=null) {
+                numberTextView.setText(phone);
+            }
+            if(photoURL!=null) {
+                Glide.with(this).load(photoURL).into(profileImageView);
+            }
         }
     }
 
     public void logOut(View view) {
         firebaseAuth.signOut();
+        goLogInScreen();
+    }
+
+    private void goLogInScreen() {
+        Intent intent = new Intent(this,LogInActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
